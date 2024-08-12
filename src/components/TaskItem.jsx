72048e8b80c9dfe28f59b780809641382a7f1f97
autovagas <bruno.guima00@gmail.com>
2024-08-12 15:50:02 -0300

    feat: ensre TaskItem changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,3 +1,6 @@
+import TaskIcon from '../assets/icons/check.svg?react'
+import ProgressIcon from '../assets/icons/progress.svg?react'
+import DetailIcon from '../assets/icons/detail.svg?react'
 const TaskItem = ({ task }) => {
   const getStatusClasses = () => {
     if (task.status === 'done') {
@@ -12,9 +15,28 @@ const TaskItem = ({ task }) => {
   }
   return (
     <div
-      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+      className={`flex items-center justify-between gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
     >
-      {task.title}
+      <div className="flex items-center gap-2">
+        {' '}
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClasses()}`}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === 'done'}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === 'done' && <TaskIcon />}
+          {task.status === 'inprogress' && (
+            <ProgressIcon className="animate-spin" />
+          )}
+        </label>
+        {task.title}
+      </div>
+      <a href="#" className="cursor-pointer transition hover:opacity-75">
+        <DetailIcon />
+      </a>
     </div>
   )
 }
