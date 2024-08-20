72a6a80cd7affe3793d815ddb841a10827997585
autovagas <bruno.guima00@gmail.com>
2024-08-19 21:05:22 -0300

    feat: ensure tasks deleted on database

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -38,7 +38,15 @@ const Tasks = () => {
 
   const eveningTasks = tasks.filter((task) => task.time === 'evening')
 
-  const handeTaskDeleteClick = (taskId) => {
+  const handeTaskDeleteClick = async (taskId) => {
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: 'DELETE',
+    })
+
+    if (!response.ok) {
+      return toast.error('Erro ao deletar tarefa!')
+    }
+
     const newTasks = tasks.filter((task) => task.id != taskId)
     setTasks(newTasks)
     toast.success('Tarefa deletada com sucesso!')
